66269421d23a5248786eef92e2eb4d611f383492
Fawaz AlTuwaijri <fawaztu@gmail.com>
2020-04-08 00:03:12 +0300

    "I thing it's finished just missing some things"

--- PCB.java
+++ PCB.java
@@ -1,2 +1,71 @@
-// GL
-// this comment is a test
+//GL
+public class PCB {
+
+    //Initializations
+    private int PID; //Process ID
+    private String PState; //Process State 
+    private int PCounter; //Process Counter (not sure)
+    private double PrSize; //Programe Size
+    private double execTime; //Expected Excution Time
+    
+    //Time attributes
+    public int numOfInstructions;
+    public int IOTime;
+    public int CPUTime;
+    public int currentIO;
+
+    public boolean cpuBound;
+
+    public PCB(int PID){
+        this.PID = PID;
+        this.PState = "new";
+        this.cpuBound = false;
+        this.PrSize = Math.random() * (204800 - 5120 + 1) + 5120; //(not sure)
+        //Missing somethings
+    }
+
+    //Setters and Getters 
+    public String getPState(){
+        return this.PState;
+    }
+
+    public void setPState(String PState){
+        this.PState = PState;
+    }
+
+
+    public int getPID() {
+		return this.PID;
+	}
+
+	public void setPID(int PID) {
+		this.PID= PID;
+	}
+
+
+	public double getPrgSize() {
+		return this.PrSize;
+	}
+
+	public void setPrgSize(double PrSize) {
+		if(PrSize >= 5120 && PrSize <= 204800)
+			this.PrSize = PrSize;
+		else 
+			System.out.printf("%d is not within limit of 5120KB to 204800KB.\n", PrSize);
+	}
+
+
+	public double getExecTime() {
+		return execTime;
+	}
+
+	public void setExecTime(double execTime) {
+		//Not sure how to do it
+	}
+    
+    
+	public int getNumOfInstructions() {
+		return this.numOfInstructions;
+	}
+
+}
