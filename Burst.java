ccaa3bd0aff8783ff0c26cda2fcd2f8ec314bddb
fro00os <55443507+fro00os@users.noreply.github.com>
2020-04-15 11:48:57 +0300

    gitignore

--- Burst.java
+++ Burst.java
@@ -3,26 +3,17 @@ public class Burst {
 
 	private Bursttype burst_type;
 	private int memory;
-	private int ioBurst;
 	
 	public Burst() {
 		this.memory = 0;
 	}
 	
-	public Burst(Bursttype burst_type, int mem) {
-		this.burst_type =Bursttype burst_type;
-		if(burst_type == cpuBurst)
+	public Burst(Bursttype b, int mem) {
+		this.burst_type = b ;
+		if(burst_type == Bursttype.cpuBurst)
 		memory = mem;
 		else
-		memory = null;
-	}
-
-	public int getCpuBurst() {
-		return cpuBurst;
-	}
-
-	public void setCpuBurst(int cpuBurst) {
-		this.cpuBurst = cpuBurst;
+		memory = 0;
 	}
 
 	public int getMemory() {
@@ -33,18 +24,21 @@ public class Burst {
 		this.memory = memory;
 	}
 
-	public int getIoBurst() {
-		return ioBurst;
+	public Bursttype getBurst_type() {
+		return burst_type;
 	}
 
-	public void setIoBurst(int ioBurst) {
-		this.ioBurst = ioBurst;
+	public void setBurst_type(Bursttype burst_type) {
+		this.burst_type = burst_type;
 	}
-	
+
 	@Override
 	public String toString() {
-		return new String("CPUBurst: "+cpuBurst+" Memory: "+memory+" IOBurst: "+ioBurst);
+		return "Burst{" +
+				"burst_type=" + burst_type +
+				", memory=" + memory +
+				'}';
 	}
-	
-	
+
+
 }
