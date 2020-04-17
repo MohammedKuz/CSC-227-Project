b21b2311f116fb354ee638dac4c0b8fcb80946fe
fro00os <55443507+fro00os@users.noreply.github.com>
2020-04-17 10:26:21 +0300

    arranged the code

--- CPU.java
+++ CPU.java
@@ -1,5 +1,6 @@
-public class CPU{
-    private Clock time;
+
+public class CPU extends Thread{
+    private Clock time; //this is never used I think it shouldn't be used cause time should be global not an object in cpu
     private RAM ram;
     private IODevice iodev;
 
@@ -13,18 +14,18 @@ public class CPU{
     @Override
     public void run() {
         //Execution cycle
-        ram.loadReadyQueue();
+        ram.loadReadyQueue(); //this method needs int parameter
         
         //sleep for 100ms
         for (int i=0; i<100;i++){
             Clock.incTime();
         }
         PCB process = ram.serveProcess();
-        int burst_time = process.getbursts().peek().getRemainingTime();
+        int burst_time = process.getCurrentBurstTime();
         //Start execution
         while (true){
             if (Clock.getTime()-100 % 200 == 0){
-                ram.loadReadyQueue(process.getProgSize());
+                ram.loadReadyQueue(process.getCurrentMemory());
             }
             // while (true){
                 // Clock.incTime();            
@@ -32,34 +33,37 @@ public class CPU{
             PCB tmp = ram.serveProcess();
             // check process not null
             
-            if(process.getbursts().peek().getRemainingTime() == 0){
-                process.getbursts().pop();
-                if(process.getbursts().isEmpty()){
-                process.terminateprocess();
-                // return process to print
-                ram.decMemorySize(process);
-                }
-                else{
-                    IODevice.addProcess(process);
-                }
-                
-            }
+
 
             if(tmp==null){
                 Clock.incTime();
                 continue;
             }
-            
-            
-            if (tmp.getbursts().peek().getRemainingTime()<burst_time){ //in case of a preemption
+
+            //in case of a preemption
+            if (tmp.getCurrentBurstTime()<burst_time){
                 process.letProcessReady();
+                process.incPreemtionCounter();
                 ram.addProcess(process);
-                // iodev.IORequest(process);
                 process = tmp;
-                burst_time = process.getbursts().peek().getRemainingTime();
+                burst_time = process.getCurrentBurstTime();
             } else {
-                ram.enqueue(tmp);
+                ram.addProcess(tmp);
             }
+
+            //when cpuBurst is done
+            if(process.getCurrentBurstTime() == 0){
+                process.getBursts().pop();
+                if(process.getBursts().isEmpty()){
+                    process.terminateProcess();
+                    // return process to print
+                    ram.decMemorySize(process);
+                }
+                else{
+                    iodev.IORequest(process);
+                }
+            }
+
             executionCycle(process);
         }
             
@@ -68,9 +72,11 @@ public class CPU{
         // }
     }
     public void executionCycle(PCB p){
-        if (p.getPStates != PStates.RUNNING)
+        if (p.getPState() != PStates.RUNNING) {
             p.letProcessRunning();
-        p.getbursts().peek().decRemainingTime();
+            p.incCPUCounter();
+        }
+        p.getBursts().peek().decRemainingtime();
         p.incCPUTime();
         Clock.incTime();
     }
